123383da5de3fab9c9e4bb2c68b2454670db5fe8
Maggie Cai <r.maggie.cai@gmail.com>
2014-09-13 13:38:48 -0400

    styled arrow and fixed the weird space on the right side

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,11 @@ $(function() {
 				var gallery_width = gallery_images.width() * gallery_images.length;
 				
 				$('#portfolio .row').css('width', gallery_width );
+
+				// var left_pos = $('#portfolio .row').width() - $('body').width();
+				// left_pos /= -2;
+
+				// $('#portfolio .row').css('left', left_pos);
 			
 			},
 			pause
@@ -41,4 +46,44 @@ var delay = (function(){
 	};
 })();
 
-//navigation
+//navigation/ button clicks
+jQuery(document).ready(function ($) {
+	//Cache some variables
+	var arrows = $('#controls button');
+	
+	arrows.click(function(e) {
+		e.preventDefault();
+		
+		if ( $(this).hasClass('inactive') )
+			return;
+		
+		var slide = null;
+		// var datasheet = $('.nav > li.active').data('slide');
+		// var offset_top = false;
+		var offset_left = false;
+		
+		
+		switch( $(this).attr('id') ) {
+			case 'left':
+				offset_left = $('#portfolio .row').offset().left + 452;
+				if ( offset_left > 0 ) {
+					offset_left = '0px';
+				}
+				break;
+			case 'right':
+				offset_left = $('#portfolio .row').offset().left - 452;
+				if ( offset_left < $('body').width() - $('#portfolio .row').width() ) {
+					offset_left = $('body').width() - $('#portfolio .row').width();
+				}
+				break;
+		}
+				
+		if ( offset_left != false ) {
+			if ( $('#portfolio .row').width() != $('body').width() ) {
+				$('#portfolio .row').stop(false, false).animate({
+					left: offset_left
+				}, 1500, 'easeInOutQuart');
+			}
+		}
+	});
+});
